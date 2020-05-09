066f737756fe9b76488444297929e6b39ac8f5fa
Romiljain258 <romiljain258@gmail.com>
2020-05-09 16:20:20 +0530

    first game

---
+++ game1.js
@@ -0,0 +1,39 @@
+
+
+    let a=9;
+    let c=0;
+    let num=document.getElementById("text");
+    for(let i=0;i<a;i++){
+        let block1=document.createElement("div");
+        block1.setAttribute("id","div"+i);
+      
+        for(let j=0;j<a;j++){
+            let block2=document.createElement("button");
+         
+            if(block2.addEventListener("click",function(){
+                 
+                if((j+i)%6!=0 && (i+j)%5!=0){
+                    block2.style.background="green";
+                    c++;
+                    document.getElementById("score").innerHTML = "<span style=\"color:white; text:20px;\">" + c + "</span>";
+                    if(50==c){
+                    window.alert("You won match");
+                    document.location.reload();
+                    }
+                   
+                 }
+                 else{
+                    block2.style.background="red";
+                    
+                    window.alert("game Over");
+                    
+                    document.location.reload();
+                 
+                 }
+                
+            }))
+            block2.setAttribute("class","bar");
+            block1.appendChild(block2);
+        }
+        num.appendChild(block1);
+    }
